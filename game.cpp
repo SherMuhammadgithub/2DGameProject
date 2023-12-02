b2481fbdb0dd31834e5097b1e55af2b32f55df10
SherMuhammadgithub <muhammadiqbalshermuhammad@gmail.com>
2023-12-03 00:42:15 +0500

    updated code

--- game.cpp
+++ game.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <windows.h>
 using namespace std;
-
 void movePlayerLeft(char[][32]);
 void movePlayerRight(char[][32]);
 void printBoard(char[][32]);
 void printFire(char[][32]);
 void moveFire(char[][32]);
 bool isDetect(char[][32]);
+void moveEnemyRight(char[][32]);
+void moveEnemyLeft(char[][32]);
+char changeDirectio(char[][32]);
+bool checkForWinner(char[][32]);
+ char direction = 'r';
 main()
 {
     char battleBoard[7][32] = {
         "###############################",
-        "#  e       e     e        e   #",
+        "#  e                          #",
         "#                             #",
         "#                             #",
         "#           p                 #",
@@ -20,16 +24,34 @@ main()
         "###############################"};
     system("cls");
     printBoard(battleBoard);
-
     while (true)
     {
-        if (GetAsyncKeyState(VK_LEFT) & 0x8000)
+        char directionOfEnemy = changeDirectio(battleBoard);
+        if (directionOfEnemy == 'r')
+        {
+            moveEnemyRight(battleBoard);
+            system("cls");
+            printBoard(battleBoard);
+        }
+        else if (directionOfEnemy == 'l')
+        {
+            moveEnemyLeft(battleBoard);
+            system("cls");
+            printBoard(battleBoard);
+        }
+        if (checkForWinner(battleBoard))
+        {
+            cout << "You win" << endl;
+            break;
+        }
+
+        if (GetAsyncKeyState(VK_LEFT))
         {
             movePlayerLeft(battleBoard);
             system("cls");
             printBoard(battleBoard);
         }
-        else if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
+        else if (GetAsyncKeyState(VK_RIGHT))
         {
             movePlayerRight(battleBoard);
             system("cls");
@@ -63,7 +85,6 @@ main()
 
 void printBoard(char arr[7][32])
 {
-
     // Print the battle board
     for (int i = 0; i < 7; i++)
     {
@@ -148,6 +169,7 @@ void moveFire(char arr[7][32])
         }
     }
 }
+// funtion for adding two numbers
 
 bool isDetect(char arr[7][32])
 {
@@ -163,4 +185,74 @@ bool isDetect(char arr[7][32])
         }
     }
     return true; // No collision detected, continue moving
+}
+void moveEnemyRight(char arr[][32])
+{
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 32; j++)
+        {
+            if (arr[i][j] == 'e' && arr[i][j + 1] == ' ') /// wall collision
+            {
+                arr[i][j] = ' ';
+                arr[i][j + 1] = 'e';
+                break;
+            }
+        }
+    }
+}
+void moveEnemyLeft(char arr[][32])
+{
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 32; j++)
+        {
+            if (arr[i][j] == 'e' && arr[i][j - 1] == ' ') /// wall collision
+            {
+                arr[i][j] = ' ';
+                arr[i][j - 1] = 'e';
+                break;
+            }
+        }
+    }
+}
+char changeDirectio(char arr[][32])
+{
+     // Initial direction is right
+
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 32; j++)
+        {
+            if (arr[i][j] == 'e')
+            {
+                if (arr[i][j + 1] == '#')
+                {
+                    direction = 'l';  // Change direction to left if wall on the right
+                }
+                else if (arr[i][j - 1] == '#')
+                {
+                    direction = 'r';  // Change direction to right if wall on the left
+                }
+                // Handle other conditions if needed
+            }
+        }
+    }
+
+    return direction;
+}
+bool checkForWinner(char arr[][32])
+{
+    bool flag = true;
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 32; j++)
+        {
+            if (arr[i][j] == 'e')
+            {
+                flag = false;
+            }
+        }
+    }
+    return flag;
 }
